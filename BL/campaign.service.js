2fa81de2e639b8baa20ad1b60640d0198d75f348
tehilla <tehilla183@gmail.com>
2024-02-04 14:03:03 +0200

    add_new_msg_service

--- BL/campaign.service.js
+++ BL/campaign.service.js
@@ -1,8 +1,19 @@
 // ייבוא הקונטרולר
 const campaignController = require('../DL/controllers/campaign.controller');
 
-async function addNewMsg(_id, body) {
-campaignController.readOne(_id)
+async function addNewMsg(id, body) {
+let campain= await campaignController.readOne(_id)
+if(!campain)throw "not campaign"
+let filter= {_id :id}
+
+if (!body.subject)throw  {code: 404 ,msg : "not message subject"} 
+if (!body.content)throw {code: 404 ,msg : "not message content"} 
+
+let messages =  {
+    subject: body.subject ,
+    content : body.content,
+}
+return await campaignController.updateOne({_id : id}, { $push: { msg: messages } }) 
 
 }
 
