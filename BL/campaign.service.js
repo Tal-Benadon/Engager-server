f235655138ad41397ea2117593317a0c60dbf413
hotam <hotamtor@gmail.com>
2024-02-04 13:46:35 +0200

    getAllCampaings @ delOneMsg

--- BL/campaign.service.js
+++ BL/campaign.service.js
@@ -1,10 +1,25 @@
 // ייבוא הקונטרולר
 const campaignController = require('../DL/controllers/campaign.controller');
 
+async function getAllCampaignsByUser(userId) {
+    // TODO: check if user exist 
+    // if (!user) throw {code:404, msg:'no user'}
+    const campaigns = await campaignController.read({_id: userId})
+    if (!campaigns) throw {code:404, msg:'no campaigns for this user'}
+    return campaigns;
+}
+
+async function delOneMessage(msgId) {
+    // const msg = await campaignController.read({_id: msgId});
+    // if (!msg) throw {}
+    const del = await campaignController.del({_id: msgId})
+    if (!del) throw {code: 404, msg: 'The deletion was not successful'}
+    return del;
+
+}
 
 
 
 
 
-
-module.exports = {}
+module.exports = {getAllCampaignsByUser, delOneMessage}
--- routes/campaign.router.js
+++ routes/campaign.router.js
@@ -5,6 +5,27 @@ const router = express.Router();
 // ייבוא השירותים
 const campaignService = require('../BL/campaign.service');
 
+router.get('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const campaigns = await campaignService.getAllCampaignsByUser(userId)
+        res.send(campaigns);
+    }
+    catch (err) {
+        res.status(err.code).send(err.msg);
+    }
+})
+
+router.delete('/:msgId', async (req, res) => {
+    try{
+        const msgId = req.params.msgId;
+        const del = await campaignService.delOneMessage(msgId);
+        res.send(del);
+    }
+    catch (err) {
+        res.status(err.code).send(err.msg);
+    }
+})
 
 
 
