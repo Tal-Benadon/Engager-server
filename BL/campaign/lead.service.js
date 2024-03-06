c1422f67acd91ad2f1b28ccaf2a2ab7e97555a2c
AviadDerli <aviad@workin.co.il>
2024-03-06 21:41:03 +0200

    merge roni 1

--- BL/campaign/lead.service.js
+++ BL/campaign/lead.service.js
@@ -1,60 +1,14 @@
-const leadController = require('../../DL/controllers/lead.controller');
-const campaignController= require('../../DL/controllers/campaign.controller')
+const leadController = require("../../DL/controllers/lead.controller");
+const campaignController = require("../../DL/controllers/campaign.controller");
 
-async function addLeadToCamp(data){
-    if(!data.campaign || !data.phone || !data.name) throw {code: 500, msg: 'User details are missing'};
-    // TODO- check if phone is valid
-
-    const phoneIsExist = await leadController.readOne({phone: data.phone});
-    if(phoneIsExist?.campaigns?.includes(data.campaign)) throw {code: 500, msg: 'The lead is already registered for this campaign'}
-    // TODO- אם הוא קיים אולי כן להחזיר לו גם אוביקט עם בנאדם
-    let mappedLead = {};
-    // אחרי בדיקות אפשר למחוק את המשתנה ורק להשאיר את הפעולה ב-אלז
-    let lastId = '';
-    if(phoneIsExist) {
-        const idToUpdate = phoneIsExist._id;
-        await leadController.update(idToUpdate, { $push: {campaigns: {_id: data.campaign}}});
-        lastId = idToUpdate;
-    } else {
-        mappedLead = {
-            phone : String(data.phone),
-            name: String(data.name),
-            campaigns: String(data.campaign),
-            //  יש דרך יותר יפה? הדיפולט בסכמה לא עובד
-            email: data.email? String(data.email): '',
-            notes: data.notes? String(data.notes): '',
-        }
-        const createdLead = await leadController.create(mappedLead); 
-        lastId = createdLead._id;
-    }
-    await campaignController.update({_id:data.campaign}, {$push: {leads: {lead: lastId}}});
-    return {campaignId: data.campaign, lead: lastId}
+async function addLeadToCamp(campId, data) {
 }
 
-async function updateLead(id, newData){
-    if(Object.keys(newData).includes('phone')){
-        const phoneIsExist = await leadController.readOne({phone: newData.phone});
-        if(phoneIsExist) throw {code: 500, msg:'phoneExist' }
-    }
-    return leadController.update(id, newData)
+async function updateLeadInCamp(campId,leadId, newData) {
 }
-
-
 
 async function delLeadFromCamp(capId, leadId) {
-    if (!capId) throw { code: 404, msg: "No campaign found" };
-    if (!leadId) throw { code: 404, msg: "No lead found" };
-    const updateIsActiv = await campaignController.updateOne(
-      { _id: capId, "leads.lead": leadId },
-      { $set: { "leads.$.isActive": false } }
-    );
-  
-    return updateIsActiv;
-  }
-
-
-
-
+}
 
 // async function getAllSentMsgs({ id_: leadId }) {
 //     const lead = await leadController.getOne({ _id: leadId })
@@ -66,8 +20,4 @@ async function delLeadFromCamp(capId, leadId) {
 //     // return lead
 // }
 
-
- 
-
-
-module.exports = {addLeadToCamp, updateLead}
+module.exports = { addLeadToCamp, updateLeadInCamp, addLeadToCamp, delLeadFromCamp };
