227bb259c40ca0cdfef7c6f466bf215a213c2174
tehilla <tehilla183@gmail.com>
2024-03-12 09:33:41 +0200

    plans wip

--- index.js
+++ index.js
@@ -2,18 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const PORT = 2500
-const campaignRouter = require('./routes/campaign.router');
-const leadRouter = require('./routes/lead.router');
-const msgRouter = require('./routes/msg.router');
-const userRouter = require("./routes/user.router");
-const loginRouter = require("./routes/login.router");
-const webhookRouter = require("./routes/webhook.router")
-const paymentRouter = require("./routes/payment.router")
-const accountRouter = require('./routes/account.router');
-const adminRouter = require('./routes/admin.router');
-const apiRouter = require ('./routes/api.router');
-const fileRouter = require( './routes/file.router');
-const whatsAppArouter = require('./routes/whatsApp.router');
+const mainRouter = require('./routes/mainRouter')
 
 const db = require('./DL/db')
 
@@ -23,21 +12,9 @@ db.connect();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'))
+app.use('/',mainRouter)
 
 
-
-app.use('/campaign', campaignRouter);
-app.use('/campaign', msgRouter);
-app.use('/lead', leadRouter);
-app.use('/user', userRouter);
-app.use('/login', loginRouter);
-app.use('/webhook', webhookRouter)
-app.use('/payment', paymentRouter);
-app.use('/accout' , accountRouter);
-app.use('/admin' , adminRouter)
-app.use('/api' , apiRouter)
-app.use('/files' , fileRouter)
-app.use('/whatsapp' , whatsAppArouter)
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
