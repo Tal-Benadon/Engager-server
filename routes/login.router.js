e7ae797bfc0f1c27d9485af67f9df0ae69e0928d
kobikru <75983501+kobikru@users.noreply.github.com>
2024-03-18 16:41:13 +0200

    ??

--- routes/login.router.js
+++ routes/login.router.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middlewares/auth")
+const auth = require("../middlewares/auth");
 
 router.post("/", async (req, res) => {
-    try {
-        console.log(req.body)
-        const phone = req.body.phone
-        const password = req.body.password
-        console.log(phone, password)
-        const login = await auth.login(phone, password);
+  try {
+    console.log(req.body);
+    const phone = req.body.phone;
+    const password = req.body.password;
+    console.log(phone, password);
+    const login = await auth.login(phone, password);
 
-        res.send(login)
+    res.send(login);
+  } catch (err) {
+    console.log(err);
+    // res.status(401).send("something went wrong");
+    res
+      .status(err.code || 401)
+      .send({ msg: err.msg || "something went wrong" });
+  }
+});
 
-    } catch (err) {
-        console.log(err)
-        // res.status(401).send("something went wrong");
-        res.status((err.code) || 401).send({msg: err.msg || 'something went wrong'});
-
-    }
-})
-
-
-
-
-module.exports = router
+module.exports = router;
--- routes/whatsApp.router.js
+++ routes/whatsApp.router.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-
-
-
 // get all leads from WhatsApp ---VV-----------------
 /**
  * @swagger
@@ -34,21 +31,23 @@ const router = express.Router();
  *         description: Internal server error
  */
 
-router.get('/whatsapp/camp/:idCamp/msg/:msgId/leads', async (req, res) => {
-    try {
-        const idCamp = req.params.idCamp;
-        const msgId = req.params.msgId;
-        const msg = await campaignService.sendSpecificMsgToCampaignLeads(idCamp, msgId, "05057095558")
-        res.send(msg);
-    } catch (err) {
-        console.log({ err });
-        res.status(err.code || 500).send({ msg: err.msg || 'something went wrong' });
-    }
+router.get("/whatsapp/camp/:idCamp/msg/:msgId/leads", async (req, res) => {
+  try {
+    const idCamp = req.params.idCamp;
+    const msgId = req.params.msgId;
+    const msg = await campaignService.sendSpecificMsgToCampaignLeads(
+      idCamp,
+      msgId,
+      "05057095558"
+    );
+    res.send(msg);
+  } catch (err) {
+    console.log({ err });
+    res
+      .status(err.code || 500)
+      .send({ msg: err.msg || "something went wrong" });
+  }
 });
-
-
-
-
 
 // function for whatsapp single lead -VV------------------------
 /**
@@ -87,42 +86,48 @@ router.get('/whatsapp/camp/:idCamp/msg/:msgId/leads', async (req, res) => {
  *         description: Custom error code for the specific scenario
  */
 
-router.get('/whatsapp/camp/:idCamp/msg/:msgId/lead/:leadId', async (req, res) => {
+router.get(
+  "/whatsapp/camp/:idCamp/msg/:msgId/lead/:leadId",
+  async (req, res) => {
     try {
-        // TODO funcion to the middleware
+      // TODO funcion to the middleware
 
-        // if(user.subscription !== "trial" && user.subscription !== "active")throw { code: 480, msg: "The user without proper authorization" };
+      // if(user.subscription !== "trial" && user.subscription !== "active")throw { code: 480, msg: "The user without proper authorization" };
 
-        const idCamp = req.params.idCamp;
-        const msgId = req.params.msgId;
-        const leadId = req.params.leadId;
+      const idCamp = req.params.idCamp;
+      const msgId = req.params.msgId;
+      const leadId = req.params.leadId;
 
-        const msg = await campaignService.getMsgAndLead(idCamp, msgId, leadId)
-        res.send(msg);
-    }
-    catch (err) {
-        res.status(444).send(err.msg);
-
-    }
-});
-
-
-
-
-router.put('/whatsapp/camp/:campId/msg/:msgId/lead/:leadId/newStatus/:newStatus', async (req, res) => {
-    try {
-        // TODO funcion to the middleware
-        // if(user.subscription !== "trial" && user.subscription !== "active")throw { code: 480, msg: "The user without proper authorization" };
-        const campId = req.params.campId;
-        const msgId = req.params.msgId;
-        const leadId = req.params.leadId;
-        const newStatus = req.params.newStatus;
-        const ans = await campaignService.updateStatusMsgOfOneLead(campId, msgId, leadId, newStatus);
-        res.send(ans);
+      const msg = await campaignService.getMsgAndLead(idCamp, msgId, leadId);
+      res.send(msg);
     } catch (err) {
-        console.log(err);
-        res.status(405).send(err.msg);
+      res.status(444).send(err.msg);
     }
-})
+  }
+);
 
-module.exports = router
+router.put(
+  "/whatsapp/camp/:campId/msg/:msgId/lead/:leadId/newStatus/:newStatus",
+  async (req, res) => {
+    try {
+      // TODO funcion to the middleware
+      // if(user.subscription !== "trial" && user.subscription !== "active")throw { code: 480, msg: "The user without proper authorization" };
+      const campId = req.params.campId;
+      const msgId = req.params.msgId;
+      const leadId = req.params.leadId;
+      const newStatus = req.params.newStatus;
+      const ans = await campaignService.updateStatusMsgOfOneLead(
+        campId,
+        msgId,
+        leadId,
+        newStatus
+      );
+      res.send(ans);
+    } catch (err) {
+      console.log(err);
+      res.status(405).send(err.msg);
+    }
+  }
+);
+
+module.exports = router;
