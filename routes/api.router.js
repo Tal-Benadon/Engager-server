0004609024b3e627bf26c15842a24025eeeef053
AviadDerli <aviad@workin.co.il>
2024-03-20 16:28:20 +0200

    webhook

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const leadServices = require('../BL/lead.service')
+const jwt = require("jsonwebtoken")
+
 
 // new lead by webhook from landing-page
-router.post("/webhook/:campId", async (req, res) => {
+router.post("/webhook/:token", async (req, res) => {
     try {
-  
+      
+      const payload = jwt.verify(req.params.token, process.env.SECRET);
+      const userId = payload.userId
+      const campId = payload.campaignId
+
+      let result = await leadServices.addLeadToCamp(campId, userId,req.body)
+      res.send(result)
     } catch (err) {
       res
         .status(err.code || 500)
--- routes/webhook.router.js
+++ routes/webhook.router.js
@@ -4,7 +4,7 @@ const auth = require("../middlewares/auth")
 // יצירת טוקן
 router.post('/', async (req, res) => {
     try {
-        res.send(await auth.createToken(req.body.campaign_id, req.body.user_id))
+        res.send(await auth.createToken(req.body.campId, req.body.userId))
     } catch (error) {
         console.log(error);
         res.status(543).send(error.msg)
@@ -12,14 +12,6 @@ router.post('/', async (req, res) => {
 })
 
 
-
-router.post('/', async (req, token, res) => {
-    const payload = jwt.verify(token, process.env.SECRET);
-    const userId = payload.userId
-    const campId = payload.campId
-
-
-})
 
 module.exports = router
 
