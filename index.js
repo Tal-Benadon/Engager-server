8cf7054075f02338251427b6869a567643610b6a
kobikru <75983501+kobikru@users.noreply.github.com>
2024-05-12 11:21:45 +0300

    before yos

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const PORT = 2500
-const mainRouter = require('./routes/mainRouter')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const PORT = 2500;
+const mainRouter = require("./routes/mainRouter");
 
-const db = require('./DL/db')
+const db = require("./DL/db");
 
 const app = express();
 db.connect();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use('/', mainRouter)
+app.use("/", mainRouter);
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerDocument = require('./swagger.json');
-const { maxCamp } = require('./middlewares/plans');
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerDocument = require("./swagger.json");
+const { maxCamp } = require("./middlewares/plans");
 
 const options = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: { title: 'How to use Raouts', description: 'Wherever {variable} appears, remove the {} and add : before' },
-        servers: [{ url: 'http://localhost:3000', }],
-        tags: [{ name: 'Campaign', }, { name: 'Message', }, { name: 'Lead', }, { name: 'WhatsApp', }],
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "How to use Raouts",
+      description:
+        "Wherever {variable} appears, remove the {} and add : before",
     },
-    apis: ['./routes/*.router.js'],
+    servers: [{ url: "http://localhost:3000" }],
+    tags: [
+      { name: "Campaign" },
+      { name: "Message" },
+      { name: "Lead" },
+      { name: "WhatsApp" },
+    ],
+  },
+  apis: ["./routes/*.router.js"],
 };
-
 
 const swaggerSpec = swaggerJSDoc(options);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.listen(PORT, () => console.log(`****server is listening on ${PORT}****`))
+app.listen(PORT, () => console.log(`****server is listening on ${PORT}****`));
